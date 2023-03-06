54b771fe368660e37876b2ec3c4787f2481ec2d7
David Francisco <davidfco.pozo@gmail.com>
2023-03-06 17:39:14 -0400

    Update request hook
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

---
+++ client/src/hooks/useUpdateRequest.js
@@ -0,0 +1,18 @@
+import { useMutation } from "react-query";
+import axios from "axios";
+
+function useUpdateRequest(url, onSuccess, onError, onMutate) {
+  const updateExpenseMutation = useMutation(
+    async ({ id, data }) => {
+      const response = await axios.put(`${url}/${id}`, data);
+      return response;
+    },
+    { onSuccess: onSuccess },
+    { onError: onError },
+    { onMutate: onMutate }
+  );
+
+  return updateExpenseMutation;
+}
+
+export default useUpdateRequest;
