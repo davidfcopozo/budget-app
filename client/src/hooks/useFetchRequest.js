d171a19806602fbb440415accd0bd7e4b9dbc532
David Francisco <davidfco.pozo@gmail.com>
2023-03-09 11:27:45 -0400

    Function to capture current user id & making the fetch request depandant of the id
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- client/src/hooks/useFetchRequest.js
+++ client/src/hooks/useFetchRequest.js
@@ -1,18 +1,30 @@
 import { useQuery } from "react-query";
 import axios from "axios";
+import { useAuth } from "../contexts/AuthContext";
+import { useState } from "react";
 
-function useFetchRequest(key, url, options = {}) {
+function useFetchRequest(key, url) {
+  const { currentUser } = useAuth();
+
+  const [currentUserId, setCurrentUserId] = useState(undefined);
+  async function getUserId() {
+    setCurrentUserId(await currentUser?.uid);
+  }
+  getUserId();
+
   const { data, error, isLoading, isFetching, refetch } = useQuery(
     {
       queryKey: key,
       queryFn: async () => {
-        const response = await axios.get(url, options);
+        const response = await axios.get(url, {
+          headers: { uid: `${currentUserId}` },
+        });
         return response.data;
       },
+      enabled: !!currentUserId,
     },
     {
       staleTime: 30000,
-      ...options,
     }
   );
   return { data, error, isLoading, isFetching, refetch };
