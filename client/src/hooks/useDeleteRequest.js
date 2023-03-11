c6b457283a07ba251c1b9b30048329821b35b65f
David Francisco <davidfco.pozo@gmail.com>
2023-03-11 13:17:20 -0400

    Passing the idToken and current user uid through headers and making the useQuery dependant of them
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- client/src/hooks/useDeleteRequest.js
+++ client/src/hooks/useDeleteRequest.js
@@ -1,13 +1,32 @@
 import { useMutation } from "react-query";
 import axios from "axios";
+import { useAuth } from "../contexts/AuthContext";
+import { useEffect, useState } from "react";
 
-function useDeleteRequest(url, onSuccess, onError, onMutate) {
+function useDeleteRequest(url, onSuccess, onError, onMutate, idToken) {
+  const { currentUser } = useAuth();
+
+  const [currentUserId, setCurrentUserId] = useState("");
+
+  useEffect(async () => {
+    getUserId();
+  }, [url, currentUser, idToken]);
+
+  async function getUserId() {
+    setCurrentUserId(await currentUser?.uid);
+  }
+
   const deleteExpenseMutation = useMutation(
     async (id) => {
-      const response = await axios.delete(`${url}/${id}`);
+      const response = await axios.delete(`${url}/${id}`, {
+        headers: {
+          uid: `${currentUserId}`,
+          Authorization: `Bearer ${idToken}`,
+        },
+      });
       return response;
     },
-    { onSuccess: onSuccess },
+    { onSuccess: onSuccess, enabled: !!currentUserId && !!idToken },
     { onError: onError },
     { onMutate: onMutate }
   );
