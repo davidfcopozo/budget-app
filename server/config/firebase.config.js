328bfad4aa5b0ac11dcff00516a8f2fe1f3ecb15
David Francisco <davidfco.pozo@gmail.com>
2023-03-11 17:39:08 -0400

    Ready to deploy
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- server/config/firebase.config.js
+++ server/config/firebase.config.js
@@ -1,18 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccountKey = require("./etc/secrets/serviceAccountKey.json");
-
-const serviceAccount = {
-  type: REACT_APP_TYPE,
-  project_id: REACT_APP_PROJECT_ID,
-  private_key_id: REACT_APP_PRIVATE_KEY_ID,
-  private_key: REACT_APP_PRIVATE_KEY,
-  client_email: REACT_APP_CLIENT_EMAIL,
-  client_id: REACT_APP_CLIENT_ID,
-  auth_uri: REACT_APP_AUTH_URI,
-  token_uri: REACT_APP_TOKEN_URI,
-  auth_provider_x509_cert_url: REACT_APP_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: REACT_APP_CLIENT_X509_CERT_URL,
-};
+const serviceAccountKey = require("../serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey),
