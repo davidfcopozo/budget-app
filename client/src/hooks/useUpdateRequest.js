70f1bd26ca44d9e10a89a0111699be41ec5a0963
David Francisco <davidfco.pozo@gmail.com>
2023-03-11 13:18:56 -0400

    Passing the idToken through headers and making the useQuery dependant of it
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- client/src/hooks/useUpdateRequest.js
+++ client/src/hooks/useUpdateRequest.js
@@ -1,13 +1,17 @@
 import { useMutation } from "react-query";
 import axios from "axios";
 
-function useUpdateRequest(url, { onSuccess, onError, onMutate }) {
+function useUpdateRequest(url, { onSuccess, onError, onMutate, idToken }) {
   const updateExpenseMutation = useMutation(
     async ({ id, data }) => {
-      const response = await axios.put(`${url}/${id}`, data);
+      const response = await axios.put(`${url}/${id}`, data, {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      });
       return response;
     },
-    { onSuccess: onSuccess },
+    { onSuccess: onSuccess, enabled: !!idToken },
     { onError: onError },
     { onMutate: onMutate }
   );
