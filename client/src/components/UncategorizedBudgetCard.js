c34ebb107bc4cf77a874b88fd01d64ff071b323a
David Francisco <davidfco.pozo@gmail.com>
2023-03-11 12:51:40 -0400

    Unused dependencies and comments removed
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- client/src/components/UncategorizedBudgetCard.js
+++ client/src/components/UncategorizedBudgetCard.js
@@ -3,7 +3,6 @@ import BudgetCard from "./BudgetCard";
 import { UNCATEGORIZED_BUDGET, useBudgets } from "../contexts/BudgetsContext";
 import { useDynamicLang } from "../contexts/LanguageContext";
 import { content } from "../components/Languages";
-import useFetchRequest from "../hooks/useFetchRequest";
 
 export default function UncategorizedBudgetCard({ ...props }) {
   const lang = useDynamicLang();
@@ -14,8 +13,6 @@ export default function UncategorizedBudgetCard({ ...props }) {
     (total, expense) => total + expense?.amount,
     0
   );
-
-  //console.log(amount);
 
   if (amount === 0) return null;
 
