658794c148098b2dc0966f1bf0f38645fc08eadd
David Francisco <davidfco.pozo@gmail.com>
2023-03-11 13:17:48 -0400

    Passing the idToken and current user uid through headers and making the useQuery dependant of them
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- client/src/hooks/useFetchRequest.js
+++ client/src/hooks/useFetchRequest.js
@@ -1,27 +1,39 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 import { useAuth } from "../contexts/AuthContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { auth } from "../config/firebase.config";
 
 function useFetchRequest(key, url) {
   const { currentUser } = useAuth();
 
-  const [currentUserId, setCurrentUserId] = useState(undefined);
+  const [idToken, setIdToken] = useState("");
+  const [currentUserId, setCurrentUserId] = useState("");
+
+  useEffect(async () => {
+    getUserId();
+  }, [key, url, currentUser, idToken]);
+
   async function getUserId() {
     setCurrentUserId(await currentUser?.uid);
+    await auth?.currentUser
+      ?.getIdToken(/* forceRefresh */ true)
+      .then((token) => setIdToken(token));
   }
-  getUserId();
 
   const { data, error, isLoading, isFetching, refetch } = useQuery(
     {
       queryKey: key,
       queryFn: async () => {
         const response = await axios.get(url, {
-          headers: { uid: `${currentUserId}` },
+          headers: {
+            uid: `${currentUserId}`,
+            Authorization: `Bearer ${idToken}`,
+          },
         });
         return response.data;
       },
-      enabled: !!currentUserId,
+      enabled: !!currentUserId && !!idToken,
     },
     {
       staleTime: 30000,
