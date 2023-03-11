bd1a0c1bed4d48bfe58016bd129fdfaac6acbf36
David Francisco <davidfco.pozo@gmail.com>
2023-03-11 13:20:56 -0400

    Auth route removed
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { login, signup } = require("../controllers/auth");
-
-router.route("/login").get(login);
-router.route("/signup").get(signup);
-
-module.exports = router;
