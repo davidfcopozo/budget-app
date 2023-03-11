eb7a013f70f5f73ded9be2a41c015eeaadc1f168
David Francisco <davidfco.pozo@gmail.com>
2023-03-11 13:00:32 -0400

    Update display name and picture feature added and disable update profile feature for dummy account
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- client/src/components/UpdateProfile.js
+++ client/src/components/UpdateProfile.js
@@ -1,29 +1,66 @@
 import React, { useRef, useState } from "react";
-import { Form, Card, Button, Alert } from "react-bootstrap";
+import { Form, Card, Button, Alert, Image } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { useTheme } from "../contexts/ThemesContext";
+import { ProfileIcon } from "./Icons";
 
 export const UpdateProfile = () => {
+  const nameRef = useRef();
+  const imageRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
-  const { currentUser, emailUpdate, passwordUpdate } = useAuth();
+  const { currentUser, emailUpdate, passwordUpdate, profileUpdate } = useAuth();
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState(null);
   const navigate = useNavigate();
+  const containerDark = useTheme();
+  const dummyUser = "fjxMskcdIxWnT5XRO6upKJOFOPS2";
+
+  let avatar = {
+    color: containerDark ? "white" : "#000",
+  };
+
+  function handleFileInputChange(e) {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  }
+  function handleImageRemoval() {
+    setFile();
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
 
+    if (currentUser.uid === dummyUser) {
+      return setError(
+        "Sorry, this is a dummy account, please create an account to be able to customize it.e"
+      );
+    }
+
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      //We return here so we can stop executing the signup since there was an error
       return setError("Passwords do not match");
     }
-    //empty array of promisses to push the requests and then use the promise.all() function in case we want to update both email and password
     const promises = [];
 
     setLoading(true);
     setError("");
+
+    if (
+      !imageRef.current.value &&
+      nameRef.current.value !== currentUser.displayName
+    ) {
+      promises.push(profileUpdate(nameRef.current.value));
+    } else if (
+      !nameRef.current.value &&
+      imageRef.current.value.name !== currentUser.photoURL
+    ) {
+      promises.push(profileUpdate(file));
+    } else {
+      promises.push(profileUpdate(nameRef.current.value, file));
+    }
 
     if (emailRef.current.value !== currentUser.email) {
       promises.push(emailUpdate(emailRef.current.value));
@@ -51,7 +88,67 @@ export const UpdateProfile = () => {
               {error}
             </Alert>
           )}
+          <div className="w-100 d-flex flex-column align-items-center mb-4">
+            {file ? (
+              <Image
+                src={URL.createObjectURL(file)}
+                roundedCircle={true}
+                fluid={true}
+                style={{ height: 130, width: 130 }}
+              />
+            ) : currentUser?.photoURL ? (
+              <Image
+                src={currentUser.photoURL}
+                roundedCircle={true}
+                fluid={true}
+                style={{ height: 130, width: 130 }}
+              />
+            ) : (
+              <ProfileIcon color={avatar.color} w="130" h="130" />
+            )}
+            <label
+              className={`mt-2 text-${
+                currentUser.uid === dummyUser ? "muted" : "primary"
+              }`}
+              style={{
+                cursor: `${
+                  currentUser.uid === dummyUser ? "default" : "pointer"
+                }`,
+              }}
+              htmlFor="file"
+            >
+              Change picture
+            </label>
+            {file && (
+              <p
+                onClick={handleImageRemoval}
+                className="text-danger"
+                style={{ fontSize: 14, cursor: "pointer" }}
+              >
+                Remove picture
+              </p>
+            )}
+            <input
+              id="file"
+              type="file"
+              style={{ visibility: "hidden" }}
+              onChange={handleFileInputChange}
+              accept="image/*"
+              ref={imageRef}
+              disabled={currentUser.uid === dummyUser ? true : false}
+            />
+          </div>
           <Form onSubmit={handleSubmit}>
+            <Form.Group id="name">
+              <Form.Label>Display Name</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Display name"
+                ref={nameRef}
+                defaultValue={currentUser?.displayName}
+                disabled={currentUser.uid === dummyUser ? true : false}
+              />
+            </Form.Group>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control
@@ -59,6 +156,7 @@ export const UpdateProfile = () => {
                 placeholder="example@email.com"
                 ref={emailRef}
                 defaultValue={currentUser?.email}
+                disabled={currentUser.uid === dummyUser ? true : false}
               />
             </Form.Group>
             <Form.Group id="password">
@@ -67,6 +165,7 @@ export const UpdateProfile = () => {
                 type="password"
                 ref={passwordRef}
                 placeholder="Leave blank to keep the same"
+                disabled={currentUser.uid === dummyUser ? true : false}
               />
             </Form.Group>
             <Form.Group id="password-confirm">
@@ -75,16 +174,23 @@ export const UpdateProfile = () => {
                 type="password"
                 ref={passwordConfirmRef}
                 placeholder="Leave blank to keep the same"
+                disabled={currentUser.uid === dummyUser ? true : false}
               />
             </Form.Group>
-            <Button type="submit" className="w-100 mt-2" disabled={loading}>
+            <Button
+              type="submit"
+              className="w-25 mt-2 mx-auto d-block align-self-center"
+              disabled={loading}
+            >
               Update
             </Button>
           </Form>
         </Card.Body>
       </Card>
-      <div className="w-100 text-center mt-2">
-        <Link to="/">Cancel</Link>
+      <div className="w-100 text-center mt-2 mb-4">
+        <Link to="/" className="text-danger">
+          Cancel
+        </Link>
       </div>
     </>
   );
