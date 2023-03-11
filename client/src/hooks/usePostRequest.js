541b401eeb50542b6c635c5898e828ffcc757ec5
David Francisco <davidfco.pozo@gmail.com>
2023-03-11 13:18:29 -0400

    Passing the idToken through headers and making the useQuery dependant of it
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- client/src/hooks/usePostRequest.js
+++ client/src/hooks/usePostRequest.js
@@ -1,13 +1,19 @@
 import { useMutation } from "react-query";
 import axios from "axios";
+import { auth } from "../config/firebase.config";
+import { useEffect, useState } from "react";
 
-function usePostRequest(url, onSuccess, onError, onMutate) {
+function usePostRequest(url, onSuccess, onError, onMutate, idToken) {
   const { mutate, data, status, error } = useMutation(
     async (body) => {
-      const response = await axios.post(url, body);
+      const response = await axios.post(url, body, {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      });
       return response.data;
     },
-    { onSuccess: onSuccess },
+    { onSuccess: onSuccess, enabled: !!idToken },
     { onError: onError },
     { onMutate: onMutate }
   );
