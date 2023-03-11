8df6aa57eb5e9258ac16aa04e8a5b8b51d1a26e9
David Francisco <davidfco.pozo@gmail.com>
2023-03-11 12:44:20 -0400

    Component modulirized
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,26 +1,20 @@
-import { useState } from "react";
-import { Button, Navbar, Stack } from "react-bootstrap";
+import { useLayoutEffect, useState } from "react";
+import { Button, Stack } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import AddBudgetModal from "./AddBudgetModal";
 import AddExpenseModal from "./AddExpenseModal";
-import BudgetCard from "./BudgetCard";
-import UncategorizedBudgetCard from "./UncategorizedBudgetCard";
-import TotalBudgetCard from "./TotalBudgetCard";
-import { UNCATEGORIZED_BUDGET, useBudgets } from "../contexts/BudgetsContext";
+import { useBudgets } from "../contexts/BudgetsContext";
 import ViewExpensesModal from "./ViewExpensesModal";
-import ThemeButton from "./ThemeButton";
 import { useTheme } from "../contexts/ThemesContext";
-import { LanguageSelect } from "./LanguageSelect";
 import { useDynamicLang } from "../contexts/LanguageContext";
 import { content } from "./Languages";
-import logo from "../assets/logo.png";
 import Footprint from "./Footprint";
-import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
+import LoadingScreen from "./LoadingScreen";
+import CardsContainer from "./CardsContainer";
 
 function Dashboard() {
-  /* Handles the state of "Show" prop in AddBudgetModal.js*/
   const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
-  /* Handles the state of "Show" prop in AddExpenseModal.js*/
   const [showAddExpenseModal, setShowAddExpenseModal] = useState(false);
   const [viewExpensesModalBudgetId, setViewExpensesModalBudgetId] = useState();
   const [addExpenseModalBudgetId, setAddExpenseModalBudgetId] = useState();
@@ -28,8 +22,16 @@ function Dashboard() {
   const [responsive, setResponsive] = useState(
     window.matchMedia("(max-width: 767px)").matches ? "vertical" : "horizontal"
   );
+  const [showCards, setShowCards] = useState(false);
 
-  const { budgets, getBudgetExpenses } = useBudgets();
+  const {
+    budgets,
+    expenses,
+    budgetsIsLoading,
+    budgetsIsFetching,
+    expensesIsLoading,
+    expensesIsFetching,
+  } = useBudgets();
 
   window.onresize = (event) => {
     setScreenSize(window.innerWidth);
@@ -50,11 +52,32 @@ function Dashboard() {
     setAddExpenseModalBudgetId(budgetId);
   }
 
-  //const headers = { Authorization: `Bearer ${user.accessToken}` };
+  function handleShowCards() {
+    if (
+      budgetsIsLoading === true ||
+      budgetsIsFetching === true ||
+      expensesIsLoading === true ||
+      expensesIsFetching === true
+    ) {
+      setShowCards(false);
+    } else {
+      setShowCards(true);
+    }
+  }
+
+  useLayoutEffect(() => {
+    handleShowCards();
+  }, [
+    budgets,
+    expenses,
+    budgetsIsLoading,
+    budgetsIsFetching,
+    expensesIsLoading,
+    expensesIsFetching,
+  ]);
 
   return (
     <>
-      {/* React Bootstrap container to wrap the app up */}
       <Container
         style={{
           minWidth: "100%",
@@ -62,29 +85,11 @@ function Dashboard() {
           display: "flex",
           flexDirection: "column",
           paddingBottom: "2rem",
+          paddingInline: "5%",
         }}
         className={darker}
       >
-        <Navbar size="sm" className={"justify-content-between "}>
-          <Navbar.Brand>
-            <img
-              src={logo}
-              width="120"
-              height="120"
-              className={"justify-content-start"}
-              alt="Budget Buddy"
-            />
-          </Navbar.Brand>
-          <Stack
-            direction="horizontal"
-            gap="2"
-            className="mb-4 align-items-md-center"
-          >
-            <Link to="/profile">Profile</Link>
-            <ThemeButton />
-            <LanguageSelect />
-          </Stack>
-        </Navbar>
+        <NavBar />
 
         <Stack direction={responsive} gap="2" className="mb-4 d-flex flex-wrap">
           <h1 style={titleStyle} className="me-auto title">
@@ -105,42 +110,17 @@ function Dashboard() {
             {content[lang]["buttons"]["addExpense"]}
           </Button>
         </Stack>
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColums: "repeat(auto-fill, minmax(300px, 1fr))",
-            gap: "1rem",
-            alignItems: "flex-start",
-          }}
-        >
-          {budgets?.map((budget) => {
-            const amount = getBudgetExpenses(budget._id)?.reduce(
-              (total, expense) => total + expense.amount,
-              0
-            );
-            return (
-              <BudgetCard
-                key={budget._id}
-                name={budget.name}
-                amount={amount}
-                maxExpending={budget.maxExpending}
-                onAddExpenseClick={() => openAddExpenseModal(budget._id)}
-                onViewExpenseClick={() =>
-                  setViewExpensesModalBudgetId(budget._id)
-                }
-              />
-            );
-          })}
 
-          <UncategorizedBudgetCard
-            onAddExpenseClick={openAddExpenseModal}
-            onViewExpenseClick={() =>
-              setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET)
-            }
+        {showCards === false ? (
+          <LoadingScreen />
+        ) : (
+          <CardsContainer
+            budgets={budgets}
+            openAddExpenseModal={openAddExpenseModal}
+            setViewExpensesModalBudgetId={setViewExpensesModalBudgetId}
           />
+        )}
 
-          <TotalBudgetCard style={{ marginBottom: "10rem" }} />
-        </div>
         <AddBudgetModal
           show={showAddBudgetModal}
           handleClose={() => setShowAddBudgetModal(false)}
