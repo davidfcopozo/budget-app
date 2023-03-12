b919408f9f1e99a61887734744b09beb02b4c95b
David Francisco <davidfco.pozo@gmail.com>
2023-03-12 07:49:04 -0400

    Ready to deploy
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- server/config/firebase.config.js
+++ server/config/firebase.config.js
@@ -1,8 +1,13 @@
 const admin = require("firebase-admin");
-const serviceAccountKey = require("../serviceAccountKey.json");
+const serviceAccountKey = require("./serviceAccountKey.json");
+require("dotenv").config;
+
+const { REACT_APP_SERVICE_ACCOUNT_KEY } = process.env;
+
+const parsedServiceAccount = JSON.parse(REACT_APP_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountKey),
+  credential: admin.credential.cert(parsedServiceAccount),
 });
 
 module.exports = admin;
