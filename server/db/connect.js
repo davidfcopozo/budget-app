18e022984995f9be63b11df9da693cdaba8886d7
David Francisco <davidfco.pozo@gmail.com>
2023-03-12 09:34:06 -0400

    Ready to deploy
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDb = (uri) => {
+  mongoose.set("strictQuery", false);
   return mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
